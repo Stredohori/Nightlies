40031fbe63ba0f14fc55893b50d63449aa4c311a
Ph42oN <julle.ys.57@gmail.com>
2024-01-10 14:33:39 +0200

    make build fail if patch fails to apply

--- build-gplasync.sh
+++ build-gplasync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 git clone --recursive https://github.com/doitsujin/dxvk.git
 #update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
 #update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
